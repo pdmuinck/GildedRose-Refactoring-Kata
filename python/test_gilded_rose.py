4c23a02bccfb4181096fa9243fa5482e55a28341
DH43EFd <pieterjan.de.muinck@ing.com>
2020-10-17 01:06:39 +0200

    modify tests

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -14,19 +14,26 @@ class GildedRoseTest(unittest.TestCase):
     def test_at_the_end_of_each_day_sell_in_and_quality_decreases_for_normal_item(self):
         """
         This method will test whether sell_in and quality of Item is decreased
-        with the specified unit.
+        when update_quality() is called.
+        V2 version results are compared with original version results.
         """
-        items = [NormalItem("foo", 1, 1)]
+        items = [Item("foo", 1, 1)]
+        items_v2 = [NormalItem("foo", 1, 1)]
         gilded_rose = GildedRose(items)
+        gilded_rose_v2 = GildedRose(items_v2)
         gilded_rose.update_quality()
+        gilded_rose_v2.update_quality_v2()
+        # test original code
         self.assertEqual(0, items[0].sell_in)
         self.assertEqual(0, items[0].quality)
+        # compare v2 with original
+        self._compare_versions(items[0], items_v2[0]) 
 
     def test_expired_normal_item_degrades_faster(self):
         """
         This method tests a normal item to degrade faster when it is expired.
         """
-        items = [NormalItem("foo", 0, 4)]
+        items = [Item("foo", 0, 4)]
         gided_rose = GildedRose(items)
         gided_rose.update_quality()
         self.assertEqual(2, items[0].quality)
@@ -37,10 +44,10 @@ class GildedRoseTest(unittest.TestCase):
         This method test that the NormalItem object doesn't go below 0 for
         it's quality field
         """
-        items = [NormalItem("foo", 0, -5), NormalItem("foo", 0, 0)]
+        items = [Item("foo", 0, -5), Item("foo", 0, 0)]
         gided_rose = GildedRose(items)
         gided_rose.update_quality()
-        self.assertEqual(0, items[0].quality)
+        self.assertEqual(-5, items[0].quality)
         self.assertEqual(0, items[1].quality)
 
     def test_aged_brie_item_increases_in_quality_the_older_it_gets(self):
@@ -48,7 +55,10 @@ class GildedRoseTest(unittest.TestCase):
         Method that tests whether a durable item increases in quality the 
         older it gets.
         """
-        items = [AgedBrieItem("foo", 1, 10), TicketItem("bar", 10, 10), TicketItem("pj", 4, 10), TicketItem("banana", 0, 10)]
+        items = [Item("Aged Brie", 1, 10), 
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 10), 
+            Item("Backstage passes to a TAFKAL80ETC concert", 4, 10), 
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
         gided_rose = GildedRose(items)
         gided_rose.update_quality()
         self.assertEqual(11, items[0].quality)
@@ -66,7 +76,7 @@ class GildedRoseTest(unittest.TestCase):
             upper_bound (int, optional): the maximum bound. Defaults to 50.
         """
         more_than_bound = upper_bound + 10
-        items = [NormalItem("foo", 10, more_than_bound), NormalItem("bar", 10, upper_bound)]
+        items = [Item("foo", 10, more_than_bound), Item("bar", 10, upper_bound)]
         gided_rose = GildedRose(items)
         gided_rose.update_quality()
         self.assertEqual(more_than_bound - 1, items[0].quality)
@@ -77,18 +87,17 @@ class GildedRoseTest(unittest.TestCase):
         This method tests that the legendary items have a fixed amount of 
         quality.
         """
-        items = [LegendaryItem("Sulfuras, Hand of Ragnaros", 10, 80), LegendaryItem("Sulfuras, Hand of Ragnaros", 10, 90)]
+        items = [Item("Sulfuras, Hand of Ragnaros", 10, 80), Item("Sulfuras, Hand of Ragnaros", 10, 90)]
         gided_rose = GildedRose(items)
         gided_rose.update_quality()
         self.assertEqual(80, items[0].quality)
         self.assertEqual(90, items[1].quality)
         self.assertEquals(10, items[0].sell_in)
 
-    def test_conjured_item_degrades_two_times_faster_than_normal_item(self):
-        items = [ConjuredItem("foo", 3, 4)]
-        gided_rose = GildedRose(items)
-        gided_rose.update_quality()
-        self.assertEquals(2, items[0].quality)
+    def _compare_versions(self, item_x, item_y):
+        self.assertEqual(item_x.name, item_y.name)
+        self.assertEqual(item_x.sell_in, item_y.sell_in)
+        self.assertEqual(item_x.quality, item_y.quality)
 
 
 
